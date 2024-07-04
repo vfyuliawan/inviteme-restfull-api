0bb1fdc5c1983f5ee2bcd58826bea3dc21e705cd
vicky fadilla <vicky.yuliawan@ist.id>
2024-07-04 19:50:33 +0700

    add cors mapping

--- src/main/java/inviteme/restfull/WebConfiguration.java
+++ src/main/java/inviteme/restfull/WebConfiguration.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import inviteme.restfull.resolver.UserArgumentResolver;
@@ -18,6 +19,13 @@ public class WebConfiguration implements WebMvcConfigurer{
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
         resolvers.add(userArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 
 }
