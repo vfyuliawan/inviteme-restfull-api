d1b23346ef4b04f210ca27bc42bd8332c69b7eaa
vicky fadilla <vicky.yuliawan@ist.id>
2024-08-26 20:26:24 +0700

    fix pom and config

--- src/main/java/inviteme/restfull/config/ApplicationConfig.java
+++ src/main/java/inviteme/restfull/config/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package inviteme.restfull.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -8,9 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.server.ResponseStatusException;
--- src/main/java/inviteme/restfull/model/response/ProjectInquiryResponse.java
+++ src/main/java/inviteme/restfull/model/response/ProjectInquiryResponse.java
@@ -1,9 +1,7 @@
 package inviteme.restfull.model.response;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-import inviteme.restfull.model.response.ProjectResponse.ThemeResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
